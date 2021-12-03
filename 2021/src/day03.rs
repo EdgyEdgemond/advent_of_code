6899861d1156f86e82462eaec2721a3c3a4abc71
EdgyEdgemond <edgy.edgemond@gmail.com>
2021-12-03 11:35:08 +0000

    day3

---
+++ 2021/src/day03.rs
@@ -0,0 +1,144 @@
+// use std::collections::HashMap;
+
+
+fn question_one(commands: &[String]) -> anyhow::Result<u32> {
+    let size = commands[0].len() as u32;
+    let mut vec: Vec<i32> = Vec::with_capacity(size as usize);
+    for _ in 0..size {
+        vec.push(0);
+    }
+
+    for command in commands {
+        for (i, c) in command.chars().enumerate() {
+            match c {
+                '1' => vec[i] += 1,
+                '0' => vec[i] -= 1,
+                _ => panic!(),
+            }
+        }
+    }
+
+    let mut gamma_rate = String::from("");
+    let mut epsilon_rate = String::from("");
+    for v in vec.iter() {
+        match v > &0 {
+            true => {
+                gamma_rate.push('1');
+                epsilon_rate.push('0');
+            },
+            false => {
+                gamma_rate.push('0');
+                epsilon_rate.push('1');
+            },
+        }
+    }
+    let gamma_val = u32::from_str_radix(gamma_rate.as_str(), 2).unwrap();
+    let epsilon_val = u32::from_str_radix(epsilon_rate.as_str(), 2).unwrap();
+
+    Ok(gamma_val * epsilon_val)
+}
+
+
+fn question_two(commands: &[String]) -> anyhow::Result<u32> {
+    let size = commands[0].len() as u32;
+    let mut o_filter = String::from("");
+    let mut o_rating = 0;
+    for i in 0..size + 1 {
+        let mut char_rate = 0;
+        let new_commands = commands
+            .iter()
+            .filter(|c| c.starts_with(o_filter.as_str()))
+            .map(|c| c.parse())
+            .collect::<Result<Vec<String>, _>>()?;
+
+        if new_commands.len() == 1 {
+            o_rating = u32::from_str_radix(new_commands[0].as_str(), 2).unwrap();
+            break
+        }
+        for command in new_commands {
+            let c = command.chars().nth(i as usize).unwrap();
+            match c {
+                '1' => char_rate += 1,
+                '0' => char_rate -= 1,
+                _ => panic!(),
+            }
+        }
+        match char_rate >= 0 {
+            true => o_filter.push('1'),
+            false => o_filter.push('0'),
+        }
+    }
+    
+    let mut co2_filter = String::from("");
+    let mut co2_rating = 0;
+    for i in 0..size + 1 {
+        let mut char_rate = 0;
+        let new_commands = commands
+            .iter()
+            .filter(|c| c.starts_with(co2_filter.as_str()))
+            .map(|c| c.parse())
+            .collect::<Result<Vec<String>, _>>()?;
+
+        if new_commands.len() == 1 {
+            co2_rating = u32::from_str_radix(new_commands[0].as_str(), 2).unwrap();
+            break
+        }
+        for command in new_commands {
+            let c = command.chars().nth(i as usize).unwrap();
+            match c {
+                '1' => char_rate += 1,
+                '0' => char_rate -= 1,
+                _ => panic!(),
+            }
+        }
+        match char_rate >= 0 {
+            true => co2_filter.push('0'),
+            false => co2_filter.push('1'),
+        }
+    }
+
+    Ok(o_rating * co2_rating)
+}
+
+fn get_input(path: &str) -> anyhow::Result<Vec<String>> {
+    Ok(std::fs::read_to_string(path)?
+        .lines()
+        .map(|l| l.parse())
+        .collect::<Result<Vec<String>, _>>()?)
+}
+
+fn run() -> anyhow::Result<()> {
+    let input = get_input("input/day03.txt")?;
+    println!("D3Q1: {}", question_one(&input)?);
+    println!("D3Q2: {}", question_two(&input)?);
+
+    Ok(())
+}
+
+pub fn main() {
+    if let Err(e) = run() {
+        panic!("{:?}", e);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test;
+    use super::*;
+
+    #[bench]
+    fn benchmark_question_one(b: &mut test::Bencher) -> anyhow::Result<()> {
+        let commands = get_input("input/day03.txt")?;
+        b.iter(|| question_one(&commands));
+
+        Ok(())
+    }
+
+    #[bench]
+    fn benchmark_question_two(b: &mut test::Bencher) -> anyhow::Result<()> {
+        let commands = get_input("input/day03.txt")?;
+        b.iter(|| question_two(&commands));
+
+        Ok(())
+    }
+}
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -4,8 +4,10 @@ extern crate lazy_static;
 
 mod day01;
 mod day02;
+mod day03;
 
 fn main() {
     day01::main();
     day02::main();
+    day03::main();
 }
