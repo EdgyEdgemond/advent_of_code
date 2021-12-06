36aa1fa9e765e0c406b45595536c08573395fe2c
EdgyEdgemond <edgy.edgemond@gmail.com>
2021-12-06 15:56:07 +0000

    day6

---
+++ 2021/src/day06.rs
@@ -0,0 +1,72 @@
+use anyhow::anyhow;
+
+fn solution(ages: &Vec<u32>, days: u32) -> anyhow::Result<u64> {
+    let mut day_counts = vec![0 as u64; 9];
+    for age in ages {
+        day_counts[*age as usize] += 1;
+    }
+    for _ in 1..=days {
+        let new_fish = day_counts[0];
+        for i in 0..8 {
+            day_counts[i] = day_counts[i + 1]
+        }
+        day_counts[6] += new_fish;
+        day_counts[8] = new_fish;
+    }
+    return Ok(day_counts.iter().fold(0, |sum, val| sum + val))
+}
+
+fn question_one(ages: &Vec<u32>) -> anyhow::Result<u64> {
+    return solution(ages, 80)
+}
+
+fn question_two(ages: &Vec<u32>) -> anyhow::Result<u64> {
+    return solution(ages, 256)
+}
+
+fn get_input(path: &str) -> anyhow::Result<Vec<u32>> {
+    Ok(std::fs::read_to_string(path)?
+        .lines()
+        .next()
+        .ok_or_else(|| anyhow!("Could not fetch ages"))
+        .unwrap()
+        .split(',')
+        .map(|n| n.parse())
+        .collect::<Result<Vec<u32>, _>>()?)
+}
+
+fn run() -> anyhow::Result<()> {
+    let input = get_input("input/day06.txt")?;
+    println!("D6Q1: {}", question_one(&input)?);
+    println!("D6Q2: {}", question_two(&input)?);
+
+    Ok(())
+}
+
+pub fn main() {
+    if let Err(e) = run() {
+        panic!("{:?}", e);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test;
+    use super::*;
+
+    #[bench]
+    fn benchmark_question_one(b: &mut test::Bencher) -> anyhow::Result<()> {
+        let ages = get_input("input/day06.txt")?;
+        b.iter(|| question_one(&ages));
+
+        Ok(())
+    }
+
+    #[bench]
+    fn benchmark_question_two(b: &mut test::Bencher) -> anyhow::Result<()> {
+        let ages = get_input("input/day06.txt")?;
+        b.iter(|| question_two(&ages));
+
+        Ok(())
+    }
+}
