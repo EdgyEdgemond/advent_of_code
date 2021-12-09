fe11a5d4134a0d15067dc279b3341d8b6e38294b
EdgyEdgemond <edgy.edgemond@gmail.com>
2021-12-09 17:32:40 +0000

    modify day09 input to force a more difficult to detect basin

--- 2021/src/day09.rs
+++ 2021/src/day09.rs
@@ -21,8 +21,37 @@ pub fn question_one(inputs: &Vec<Vec<i32>>) -> anyhow::Result<i32> {
     Ok(total)
 }
 
+struct Point {
+    x: i32,
+    y: i32,
+    val: i32,
+}
+
 pub fn question_two(inputs: &Vec<Vec<i32>>) -> anyhow::Result<i32> {
-    Ok(0)
+    for input in inputs {
+        println!("{:?}", input);
+    }
+    let mut total = 0;
+    // Create a hashmap of all points by x, y.
+    // Link list all the points
+    // Determine basins (this is annoying given a basin can go down right up etc.
+    // Get basin size
+    let d = vec![9i32; inputs[0].len()];
+    for (i, row) in inputs.iter().enumerate() {
+        let prow = inputs.get((i as i32 - 1) as usize).or(Some(&d)).unwrap();
+        let nrow = inputs.get(i + 1).or(Some(&d)).unwrap();
+        for (j, col) in row.iter().enumerate() {
+            let left = row.get((j as i32 - 1) as usize).or(Some(&9)).unwrap();
+            let right = row.get(j + 1).or(Some(&9)).unwrap();
+            let up = prow[j];
+            let down = nrow[j];
+            // let checks = [col < left, col < right, col < &up, col < &down];
+            // if checks.iter().all(|x| *x) {
+            //     total += col + 1;
+            // }
+        }
+    }
+    Ok(total)
 }
 
 pub fn get_input(path: &str) -> anyhow::Result<Vec<Vec<i32>>> {
