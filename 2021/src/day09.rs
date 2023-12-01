02ee7512ddb3203c1f287679974f84c78c4366a7
EdgyEdgemond <edgy.edgemond@gmail.com>
2023-12-01 10:17:45 +0000

    rust day1 2023

--- 2021/src/day09.rs
+++ 2021/src/day09.rs
@@ -1,4 +1,5 @@
 use anyhow::anyhow;
+use std::collections::HashMap;
 
 pub fn question_one(inputs: &Vec<Vec<i32>>) -> anyhow::Result<i32> {
     let mut total = 0;
@@ -21,6 +22,7 @@ pub fn question_one(inputs: &Vec<Vec<i32>>) -> anyhow::Result<i32> {
     Ok(total)
 }
 
+#[derive(Debug)]
 struct Point {
     x: i32,
     y: i32,
@@ -35,21 +37,38 @@ pub fn question_two(inputs: &Vec<Vec<i32>>) -> anyhow::Result<i32> {
     // Create a hashmap of all points by x, y.
     // Link list all the points
     // Determine basins (this is annoying given a basin can go down right up etc.
-    // Get basin size
-    let d = vec![9i32; inputs[0].len()];
+    // Get basin sizes order and get top 3 sum()
+    // let d = vec![9i32; inputs[0].len()];
+    let mut map: HashMap<(i32, i32), Point> = HashMap::new();
     for (i, row) in inputs.iter().enumerate() {
-        let prow = inputs.get((i as i32 - 1) as usize).or(Some(&d)).unwrap();
-        let nrow = inputs.get(i + 1).or(Some(&d)).unwrap();
         for (j, col) in row.iter().enumerate() {
-            let left = row.get((j as i32 - 1) as usize).or(Some(&9)).unwrap();
-            let right = row.get(j + 1).or(Some(&9)).unwrap();
-            let up = prow[j];
-            let down = nrow[j];
-            // let checks = [col < left, col < right, col < &up, col < &down];
-            // if checks.iter().all(|x| *x) {
-            //     total += col + 1;
-            // }
+            if *col != 9 {
+                map.insert((i as i32, j as i32), Point {y: i as i32, x: j as i32, val: *col});
+            }
         }
+    }
+    // seen = SET
+    // Basins = SET
+    for (y, x) in map.keys() {
+        let up = map.get(&(*y - 1, *x));
+        let down = map.get(&(*y + 1, *x));
+        let left = map.get(&(*y, *x - 1));
+        let right = map.get(&(*y, *x + 1));
+
+        print!("{:?}", (y, x));
+        if up.is_some() {
+            print!(" up");
+        }
+        if left.is_some() {
+            print!(" left");
+        }
+        if down.is_some() {
+            print!(" down");
+        }
+        if right.is_some() {
+            print!(" right");
+        }
+        println!("");
     }
     Ok(total)
 }
